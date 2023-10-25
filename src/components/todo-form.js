9ef0acd9c3edf9810cde9dbfcd69f386af779d99
Bismark Adzai <adzaibismark@gmail.com>
2023-10-25 02:40:53 +0000

    first commit
    first commit

---
+++ src/components/todo-form.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react'
+
+const Todo() {
+    const [tasks, setTasks] = ([]);
+    const [task, setTask] = ('');
+
+    return (
+        <div>
+            <form onSubmit={addTask}>
+                <input
+                type='text'
+                placeholder='Add a task'
+                value={task}
+                onChange={onChange}/>
+                 <button onClick={addTask}>Add</button>
+            </form>
+
+           
+        </div>
+    )
+}
+export default Todo;
